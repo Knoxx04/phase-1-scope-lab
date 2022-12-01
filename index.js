32e0e30311e498bb6583813e745e4ce17f65260c
Knoxx04 <onyiknoxx3@gmail.com>
2022-12-01 12:55:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+} 
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overWiteBestCustomer(){
+    bestCustomer = 'Jacob';
+}
+
+
+
+const leastFavouriteCustomer = 'Tom';
+function changeLeastFavouriteCustomerName(){
+    leastFavouriteCustomerName = 'Thuku';
+}
